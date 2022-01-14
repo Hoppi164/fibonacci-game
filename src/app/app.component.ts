edaaadfc49edae4543884864c6175bbbdb9af540
Hoppi164 <hoppi164@gmail.com>
2022-01-15 01:31:37 +1000

    Added basic app and timer functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fibonacci-game';
+
+  counter:number = 0
+  timerRef:any;
+  isRunning:boolean = false;
+  
+  toggleTimer() {
+    this.isRunning = !this.isRunning;
+    const startTime = Date.now() - (this.counter || 0);
+
+    if(this.isRunning){
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+      });   
+    }
+    else{
+      clearInterval(this.timerRef)
+    }
+  }
+
+  timeTaken(){
+    let mins = Math.floor( (this.counter / 60000) )
+    let secs = Math.round( (this.counter / 1000) % 60 )
+    return `Minutes: ${ mins }\nSeconds: ${secs}`
+  }
 }
